cd1f70872fd74668cdccf1d8628842e88902235c
Andrew Nomura <drew97.nomura@gmail.com>
2020-03-23 16:14:02 -0700

    Add files via upload

---
+++ rpc.js
@@ -0,0 +1,45 @@
+"use strict"
+
+
+let express = require("express");
+let rpc = express();
+
+
+// This responds with "Hello World" on the homepage
+rpc.get('/', function(req, res) {
+	console.log("Got a GET request for the homepage");
+	res.send("Hello GET");
+})
+
+// This responds a POST request for the homepage
+rpc.post('/', function(req, res) {
+	console.log("Got a POST request for the homepage");
+	res.send("Hello POST");
+})
+
+// This ressponds a DELETE request for the /del_user page
+rpc.delete('/del_user', function(req, res) {
+	console.log("Got a DELETE request for /del_user");
+	res.send("Hello DELETE");
+})
+
+// This responds a GET request for the /list_user page
+rpc.get('/list_user', function(req, res) {
+	console.log("Got a GET request for /list_user");
+res.send("Page Listing");
+})
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+rpc.get('/ab*cd', function(req, res) {
+	console.log("Got a GET request for /ab*cd");
+	res.send("Page Patern Match");
+})
+
+
+let server = rpc.listen(8081, function () {
+	let host = server.address().address;
+	let port = server.address().port;
+
+
+	console.log("rpc listening at http://%s:%s", host, port);
+});
---
+++ server.js
@@ -0,0 +1,30 @@
+// server.js
+// loads the things we need
+
+"use strict"
+
+let express = require('express');
+let connect = require('connect');
+let serveStatic = require('serve-static');
+let rpc = express();
+
+
+connect().use(serveStatic(__dirname)).listen(8081, function() {
+	console.log('Server running on 8081...');
+});
+
+//set the view engine to ejs
+rpc.set('view engine', 'ejs');
+
+
+// index page
+rpc.get('/', function(req, res) {
+	res.render('pages/index');
+})
+
+// about page
+rpc.get('/about', function(req, res) {
+	res.render('pages/about');
+});
+
+rpc.listen(8081);
